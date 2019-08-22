ddecd70b00a8489fc7be990d28753cbd10fcdcb6
Jackie Atwood <takalaj@verizon.net>
2019-08-22 11:04:56 -0400

    Everything works... not understanding the last part of Step 2 regarding rendering and the set method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from "./components/RockerData";
+import RockerForm from "./components/RockerForm"; 
+import RockerList from "./components/RockerList";
 
 
 function App() {
+  const [thing, setThing] = useState(data);
+  const addNewRocker = form => {
+    setThing([...thing, form]);
+  };
   return (
     <div className="App">
-  
+      <h1> Favorite Rockers!! </h1>
+      <RockerForm addNewRocker={addNewRocker}/>
+      <RockerList theList={thing}/>
     </div>
   );
 }
